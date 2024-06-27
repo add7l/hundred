3ac773f53619873fb1080f5f0005f6975a2ad2b3
add7l <d93slio@outlook.com>
2024-06-27 10:37:02 +0800

    Modify middle.cpp

--- middle.cpp
+++ middle.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Subject open draw production music.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

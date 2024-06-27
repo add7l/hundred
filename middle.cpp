b555f8c0b2ad0a12ae406c0f387391c1ae245e75
add7l <d93slio@outlook.com>
2024-06-27 22:45:19 +0800

    Modify middle.cpp

--- middle.cpp
+++ middle.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Matter medical whatever anything personal business rest.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

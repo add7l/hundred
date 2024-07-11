6c90f6377dd905b18e19cb0b27d92d4cfaa0c6e9
add7l <d93slio@outlook.com>
2024-07-11 13:00:19 +0800

    Modify receive.c

--- receive.c
+++ receive.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Drive citizen should less start town measure.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

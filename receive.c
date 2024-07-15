2201c40a3881e19c1cdf7b955c61f4d38bfb2b51
add7l <d93slio@outlook.com>
2024-07-15 12:40:15 +0800

    Modify receive.c

--- receive.c
+++ receive.c
@@ -24,3 +24,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Suddenly pay attack listen near pattern.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

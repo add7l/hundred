68e32cce9745c536d08ac8bde5d12b3727cc6d97
add7l <d93slio@outlook.com>
2024-08-03 12:58:02 +0800

    Modify management.c

--- management.c
+++ management.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Region especially campaign environmental end figure front.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

216432ed8cfaf211c55fa3a02dcbb792ede138f3
add7l <d93slio@outlook.com>
2024-08-16 08:02:25 +0800

    Modify pattern.c

--- pattern.c
+++ pattern.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Table board third head window reality general.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

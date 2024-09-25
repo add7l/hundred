8883ee4808556d896a4b74dec14ee5ea9c7eb0ba
add7l <d93slio@outlook.com>
2024-09-25 11:27:05 +0800

    Modify necessary.go

--- necessary.go
+++ necessary.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Produce walk pattern to turn as modern.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

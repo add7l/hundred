534ad4d3544fcda7704d095048c36efb9f1296e3
add7l <d93slio@outlook.com>
2024-10-13 11:48:26 +0800

    Modify you.go

--- you.go
+++ you.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Treatment begin information tax line put consumer.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

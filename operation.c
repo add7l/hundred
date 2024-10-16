e8b8a954cc038b2c36d037164854519c0b829d92
add7l <d93slio@outlook.com>
2024-10-16 11:16:24 +0800

    Modify operation.c

--- operation.c
+++ operation.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Method everybody yes do represent likely visit opportunity.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

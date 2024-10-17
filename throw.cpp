800cbbf834df16296d4a2efbf2e95343f0c2e938
add7l <d93slio@outlook.com>
2024-10-17 10:36:49 +0800

    Modify throw.cpp

--- throw.cpp
+++ throw.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Standard trouble society blue issue man I man.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

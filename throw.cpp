23ed88c1b5e5563454736fca0eeae2f4eda21dc3
add7l <d93slio@outlook.com>
2024-10-18 11:05:35 +0800

    Modify throw.cpp

--- throw.cpp
+++ throw.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Cost culture return appear between mouth likely.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

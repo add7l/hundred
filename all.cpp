cea205cd06dbf26f3166aacc8130fa73e83137df
add7l <d93slio@outlook.com>
2024-11-25 16:37:21 +0800

    Modify all.cpp

--- all.cpp
+++ all.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Week receive ever thought husband special.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

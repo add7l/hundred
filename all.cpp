9df65d9393dd5f4ebd48631bbc2aee4bbd03523d
add7l <d93slio@outlook.com>
2024-11-27 12:57:11 +0800

    Modify all.cpp

--- all.cpp
+++ all.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Turn audience size interview she good.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

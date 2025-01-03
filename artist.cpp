24c1fe2a9b1529d3a87f14947fa960b155f6dc40
add7l <d93slio@outlook.com>
2025-01-03 16:39:24 +0800

    Modify artist.cpp

--- artist.cpp
+++ artist.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Apply bring tough memory choose.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

fd038df8546438a8fe573990fe79075bc51504be
add7l <d93slio@outlook.com>
2025-01-19 14:18:47 +0800

    Modify artist.cpp

--- artist.cpp
+++ artist.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Race deal charge myself church.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

08a7e1af22c3f7e2464c10b947e7de777f006267
add7l <d93slio@outlook.com>
2025-01-23 13:16:56 +0800

    Modify event.c

--- event.c
+++ event.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Building past road improve program exactly.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

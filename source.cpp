96c758d11bea8e17327f5c27a11cd20a9ed1cbdf
add7l <d93slio@outlook.com>
2025-02-19 20:52:40 +0800

    Modify source.cpp

--- source.cpp
+++ source.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Prepare central show maybe modern exactly return.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

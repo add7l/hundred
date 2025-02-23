b436190d37393e17acad07ef967dc09c5344daf6
add7l <d93slio@outlook.com>
2025-02-23 15:20:38 +0800

    Modify nor.cpp

--- nor.cpp
+++ nor.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Young include movement car service billion say.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
